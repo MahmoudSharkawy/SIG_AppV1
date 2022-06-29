35d130d4025eddce1eb8865ea8ee5765faa20b76
ostorasoft <msharkawy005@gmail.com>
2022-06-29 17:25:53 +0200

    Jframecommit

---
+++ src/SIGApp.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class SIGApp extends JFrame {
+    private JPanel mainPanel;
+    public SIGApp  (String title){
+        super(title);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setContentPane(mainPanel);
+        this.pack();
+    }
+    public static void main(String[] args) {
+        JFrame frame = new SIGApp("Sales Invoice Generator - SIG | Beta")
+    }
+}
